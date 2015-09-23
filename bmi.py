50571af6596517021e037160c74646ff91e8a8c7
tcb <tongchangbin@crazyhornets.com>
2015-09-23 16:49:58 +0800

    提交

---
+++ bmi.py
@@ -0,0 +1,16 @@
+print('计算BMI公式')
+sg = input('请输入你的身高：')
+tz = input('请输入你的体重：')
+s = float(sg)
+t = float(tz)
+bmi = t/(s*s)
+if bmi >32:
+	print('严重肥胖')
+elif 28<bmi<=32:
+	print('肥胖')
+elif 25<bmi<=28:
+	print('过重')
+elif 18.5<bmi<=25:
+	print('正常')
+else:
+	print('过轻')
